ef1565793d076e02629a4372031df31069d9b835
afmorac <andresfmora.catro@gmail.com>
2025-02-23 20:34:49 -0400

    install fix

--- install.sh
+++ install.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
 # Verificación del shell
-if [ "$SHELL" != "/bin/bash" ] && [ "$SHELL" != "/bin/zsh" ]; then
+if [[ "$SHELL" != "/bin/bash" && "$SHELL" != "/bin/zsh" ]]; then
     echo "Este script está optimizado para Bash y Zsh. Cambie su shell si es necesario."
     exit 1
 fi
 
 # Verificar si el script está siendo ejecutado con permisos de superusuario (sudo)
-if [ "$(id -u)" -ne 0 ]; then
+if [[ $(id -u) -ne 0 ]]; then
     echo "Este script debe ejecutarse con permisos de superusuario. Use 'sudo'."
     exit 1
 fi
 
-# Descargar el archivo ejecutable 's' desde el repositorio de GitHub
-echo "Descargando el archivo ejecutable 's'..."
-curl -sSL https://raw.githubusercontent.com/afmorac/holbertonschool-low_level_programming/master/sat/s -o /tmp/s
+# Definir URL del ejecutable
+EXECUTABLE_URL="https://raw.githubusercontent.com/afmorac/holbertonschool-low_level_programming/master/sat/s"
+INSTALL_PATH="/usr/local/bin/s"
+
+# Descargar el archivo ejecutable 's'
+echo "Descargando el ejecutable..."
+curl -sSL "$EXECUTABLE_URL" -o "$INSTALL_PATH"
 
 # Verificar si la descarga fue exitosa
-if [ ! -f /tmp/s ]; then
-    echo "Error: No se pudo descargar el archivo 's'."
+if [[ ! -f "$INSTALL_PATH" ]]; then
+    echo "Error: No se pudo descargar el ejecutable. Verifique la URL y su conexión a internet."
     exit 1
 fi
 
-# Mover el archivo descargado a /usr/local/bin para que sea accesible globalmente
-echo "Moviendo el archivo 's' a /usr/local/bin..."
-sudo mv /tmp/s /usr/local/bin/s
-
-# Darle permisos de ejecución
-sudo chmod +x /usr/local/bin/s
+# Dar permisos de ejecución
+chmod +x "$INSTALL_PATH"
 
 # Confirmación de instalación
 echo "Shell Automation Tool (SAT) instalado correctamente. Ahora puede usarlo globalmente con el comando 's'."
